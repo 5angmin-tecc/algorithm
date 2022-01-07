75086cd737197124b8a776e60d8acc8fad845e2e
5angmin-tecc <5angmin.tecc@gmail.com>
2022-01-07 22:09:26 +0900

    프로그래머스 level2 1 문항 해결
    숫자의 표현

---
+++ programmers/level2/숫자의 표현/solution.js
@@ -0,0 +1,18 @@
+function solution(n) {
+  var answer = 0;
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    let sum = 0;
+    for (let j = i; j <= n; j++) {
+      sum += j;
+      if (sum === n) {
+        result++;
+        break;
+      } else if (sum > n) {
+        break;
+      }
+    }
+  }
+  answer = result;
+  return answer;
+}
